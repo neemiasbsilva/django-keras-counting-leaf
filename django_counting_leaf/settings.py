0314646cae58d9b1a34d32e4c9eeed27322982b7
neemiasbsilva <neemiasbsilva@gmail.com>
2020-07-07 11:53:02 -0400

    update for deploy in heroku

--- django_counting_leaf/settings.py
+++ django_counting_leaf/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,5 +124,7 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATIC_FILESTORAGE = 'whitenoise.django.GzipManifestStaticFileStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
