f613659ebab0943bbbd7673d1a93022490e7e74c
neemiasbsilva <neemiasbsilva@gmail.com>
2020-07-08 06:16:09 -0400

    update file setting to deploy in heroku

--- django_counting_leaf/settings.py
+++ django_counting_leaf/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/static/'
 
 STATIC_FILESTORAGE = 'whitenoise.django.GzipManifestStaticFileStorage'
 
