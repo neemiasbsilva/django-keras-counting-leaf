2bf7760f86003e7286b4722dcb8f1ac1512b9054
neemiasbsilva <neemiasbsilva@gmail.com>
2020-07-08 06:38:43 -0400

    update file setting to deploy in heroku

--- django_counting_leaf/settings.py
+++ django_counting_leaf/settings.py
@@ -145,7 +145,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
+ALLOWED_HOSTS = ['*']
 STATIC_FILESTORAGE = 'whitenoise.django.GzipManifestStaticFileStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
