e9e96d4f68a4c714c274ac62c6b5c1e0882cc741
neemiasbsilva <neemiasbsilva@gmail.com>
2020-07-08 09:38:27 -0400

    update the settings

--- django_counting_leaf/settings.py
+++ django_counting_leaf/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import dj_database_url
 
+from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -116,7 +118,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
